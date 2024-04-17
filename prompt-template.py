5171cdb53358e44fdffa8b7f8f32dc835f07036a
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-04-18 00:03:16 +0530

    Create prompt-template.py

---
+++ prompt-template.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+OpenAI_API_key =os.getenv("OpenAI_API")
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+# instantiate model
+llm = ChatOpenAI(
+    api_key=OpenAI_API_key,
+    model="gpt-3.5-turbo",
+    temperature=0, 
+    max_tokens=1000
+)
+
+# prompt template
+prompt = ChatPromptTemplate("tell me a joke about {subject}")
+
+# LLM Chain
+chain = prompt | llm
+
+prompt = st.chat_input("Enter the message...")
+
+response = chain.invoke({"subject" : prompt})
+
+st.write(response)
