4a6f24c73f25861528373cb3f18fa28ebf5931f0
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-04-18 21:05:09 +0530

    parsers

--- output-parsers.py
+++ output-parsers.py
@@ -7,25 +7,77 @@ OpenAI_API_key =os.getenv("OpenAI_API")
 
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser,CommaSeparatedListOutputParser, JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+
 
 # instantiate model
 llm = ChatOpenAI(
     api_key=OpenAI_API_key,
     model="gpt-3.5-turbo",
     temperature=0, 
-    max_tokens=1000
+    max_tokens=500
 )
 
-# prompt template
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are an expert in Materials Science and Engineering. Answer the questions accordingly.")
-        ("user", {"input"})
-    ]
-)
+#output parser
+def call_string_output_parser(prompt,llm):
 
-# LLM Chain
-chain = prompt | llm
+
+    # prompt template
+    prompt = ChatPromptTemplate.from_messages(        [
+            ("system", "You are an expert in Materials Science and Engineering. Answer the questions accordingly."),
+            ("human", "{input}")
+    ])
+
+    parser = StrOutputParser()
+
+    # LLM Chain
+    chain = prompt | llm | parser
+
+    response = chain.invoke({"input" : prompt})
+
+    return response
+
+def call_list_output_parser(prompt,llm):
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Mention synonyms fro {word} in a comma separated list."),
+        ("human", "{word}")
+    ])
+
+    parser = CommaSeparatedListOutputParser()
+
+    chain = prompt | llm | parser
+
+    output = chain.invoke({
+        "word" : prompt
+    })
+
+    return output
+
+def call_json_output_parser(prompt,llm):
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Extract information from the following phrase. \nformatting instructions : {formatting instructions}"),
+        ("human", "{phrase}")
+    ])
+
+    class Person(BaseModel):
+        name : str = Field("the name of the person")
+        age : int = Field("the age of the person")
+
+
+    parser = JsonOutputParser(pydantic_object=Person)
+
+    chain = prompt | llm | parser
+
+    output = chain.invoke({
+        "phrase" : "Max is 30 years old.",
+        "format_instructions" : parser.get_format_instructions() 
+    })
+
+    return output
+
 
 st.title("MatTalks")
 
@@ -34,7 +86,6 @@ with st.sidebar:
 
 prompt = st.chat_input("Enter the message...")
 
-response = chain.invoke({"input" : prompt})
+output = call_string_output_parser(prompt,llm)
 
-
-st.write(response)
+st.write(output)
