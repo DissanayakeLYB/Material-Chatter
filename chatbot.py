7efa81b2c9ba8bf173cbc2b5d72e4f4b3de273ae
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-04-20 00:18:32 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -14,23 +14,41 @@ llm = ChatOpenAI(
     api_key=OpenAI_API_key,
     model="gpt-3.5-turbo",
     temperature=0, 
-    max_tokens=500
+    max_tokens=100
     )
 
-# front end
-st.title("MatTalks")
 
-prompt = st.chat_input("Enter the message...")
+st.title("PDFTalk")
 
-with st.sidebar:
-    st.title("Chat History")
-
-if prompt:
-
-    llm_response = llm.invoke(prompt)
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
     
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+# here ":=" will assign the chat input to prompt and also checks if True
+if prompt := st.chat_input("Talk to your PDF..."): 
+
+    # # display response of the assistant
     with st.chat_message("user"):
         st.write(prompt)
 
+    # add assistant's response to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = llm.invoke(prompt)
+
+    # display response of the assistant
     with st.chat_message("assistant"):
-        st.write(llm_response.content)
+        st.write(response.content)
+
+    # add assistant's response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response.content})
+
+with st.sidebar:
+    st.title("Upload a PDF")
+    upload_files = st.file_uploader(" ")
