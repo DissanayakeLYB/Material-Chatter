d5f2c84a2bae74ee9400b17ce37ac1d598cd6203
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-04-20 00:18:35 +0530

    Update bot-llama.py

--- bot-llama.py
+++ bot-llama.py
@@ -7,32 +7,42 @@ from llama_index.llms.llama_api import LlamaAPI
 load_dotenv()
 
 api_key = os.getenv("Llama_index_API")
+
+#instantiate model
 llm = LlamaAPI(api_key=api_key)
 
-st.title("MatChat")
 
-prompt = st.chat_input("Enter the message... ")
+st.title("PDFTalk")
 
-# chat_history = []
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+# here ":=" will assign the chat input to prompt and also checks if True
+if prompt := st.chat_input("Talk to your PDF..."): 
+
+    # # display response of the assistant
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    # add assistant's response to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = llm.invoke(prompt)
+
+    # display response of the assistant
+    with st.chat_message("assistant"):
+        st.write(response.content)
+
+    # add assistant's response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response.content})
 
 with st.sidebar:
-    st.title("Chat History")
-
-if prompt:
-
-    with st.chat_message("User"):
-        st.write(prompt)
-    
-    # chat_history.append(prompt)
-
-    response = llm.complete(prompt)
-
-    with st.chat_message("Assistant"):
-        st.write(response.text)
-
-    # chat_history.append(response)
-
-
-
-
-
+    st.title("Upload a PDF")
+    upload_files = st.file_uploader(" ")
