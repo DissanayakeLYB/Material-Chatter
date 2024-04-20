2805ce6541b0af8a83fc685b78b4e38ce01dcbda
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-04-20 22:57:42 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,14 @@ import streamlit as st
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.documents import Document
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
+from langchain.chains import create_retrieval_chain
 
 load_dotenv()
 
@@ -16,6 +24,40 @@ llm = ChatOpenAI(
     temperature=0, 
     max_tokens=100
     )
+
+def create_db(docs):
+    embedding = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embedding=embedding)
+    return vectorstore
+
+
+def create_chain(vectorstore):
+    llm = ChatOpenAI(
+        model="gpt-3.5-turbo",
+        temperature=0.2,
+        api_key= OpenAI_API_key
+    )
+
+    prompt = ChatPromptTemplate.from_template("""
+    Answer the user's question :
+    Context : {context}
+    Question : {input}
+    """)
+
+    #chain = prompt | llm 
+
+    chain = create_stuff_documents_chain(
+        llm=llm,
+        prompt=prompt
+    )
+
+    retriever = vectorstore.as_retriever(search_kwargs={"k":2})
+
+    retrieval_chain = create_retrieval_chain(
+        retriever, 
+        chain
+    )
+    return chain
 
 
 st.title("PDFTalk")
@@ -51,5 +93,5 @@ if prompt := st.chat_input("Talk to your PDF..."):
 
 with st.sidebar:
     st.title("Upload a PDF")
-    upload_files = st.file_uploader(" ")
+    upload_files = st.file_uploader(" ", type="pdf")
     st.button("Process")
